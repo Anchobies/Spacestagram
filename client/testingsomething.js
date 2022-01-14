084821997a3fab32dae1754ea5c8f13cd9144f10
Anchobies <anthonychung97@gmail.com>
2022-01-14 14:30:54 -0500

    Fixed webpack.config then deployed on Heroku

---
+++ client/testingsomething.js
@@ -0,0 +1,4 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+console.log(MiniCssExtractPlugin.default);
+console.log(CssMinimizerPlugin);
