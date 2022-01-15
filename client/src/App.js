b2a840903b63edb146f81f6bc8345576cb5c182b
Anchobies <anthonychung97@gmail.com>
2022-01-14 22:00:46 -0500

    Use Routes instead of Switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./Home";
 import Search from "./Search";
 
+// const API_KEY = process.env.NASA_API_KEY;
+
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path="/search/:query">
-        <Search />
-      </Route>
-      <Route exact path="/">
-        <Home />
-      </Route>
+      <Routes>
+        <Route exact path="/search/:query" element={<Search />} />
+        <Route exact path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 }
