1d5af7d6709b3dfc4b20db4185929a0acf2fd8d5
Anchobies <anthonychung97@gmail.com>
2022-01-19 15:55:33 -0500

    Fixed applications controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,8 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
     before_action :check_session
 
-
-  =begin 
-  Checks sessions cookie to see whether User object is already created for this user.
-  If not, new User is created and saved as sessions cookie.
-  =end
+  # Checks sessions cookie to see whether User object is already created for this user.
+  # If not, new User is created and saved as sessions cookie.
   def check_session
     if !session[:user_id] || !User.find_by(id: session[:user_id])
         user = User.create()
